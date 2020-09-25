fd8d014ddfeba724eda341742ba72a7e76dafc98
Kennith Leung <kennith.leung@gmail.com>
2020-09-24 18:13:32 -0700

    forked from tilfin

---
+++ src/content.js
@@ -0,0 +1,1 @@
+if (document.body) extendIAMFormList();
---
+++ src/options.js
@@ -0,0 +1,88 @@
+function elById(id) {
+  return document.getElementById(id);
+}
+
+window.onload = function() {
+  var colorPicker = new ColorPicker(document);
+
+  var selection = [];
+  var textArea = elById('awsConfigTextArea');
+  textArea.onselect = function() {
+    var str = this.value.substring(this.selectionStart, this.selectionEnd);
+    var r = str.match(/^([0-9a-fA-F]{6})$/);
+    if (r !== null) {
+      colorPicker.setColor(r[1]);
+      selection = [this.selectionStart, this.selectionEnd];
+      colorPicker.onpick = function(newColor) {
+        str = textArea.value;
+        textArea.value = str.substring(0, selection[0]) + newColor + str.substring(selection[1]);
+      }
+    } else {
+      selection = [];
+      colorPicker.onpick = null;
+    }
+  }
+
+  var msgSpan = elById('msgSpan');
+  var saveButton = elById('saveButton');
+  saveButton.onclick = function() {
+    var rawstr = textArea.value;
+
+    try {
+      const profiles = loadAwsConfig(rawstr);
+      if (profiles.length > 200) {
+        msgSpan.innerHTML = '<span style="color:#dd1111">Failed to save bacause the number of profiles exceeded maximum 200!</span>';
+        return;
+      }
+
+      localStorage['rawdata'] = rawstr;
+
+      const dps = new DataProfilesSplitter();
+      const dataSet = dps.profilesToDataSet(profiles);
+      dataSet.lztext = LZString.compressToUTF16(rawstr);
+
+      chrome.storage.sync.set(dataSet,
+        function() {
+          const { lastError } = chrome.runtime || browser.runtime;
+          if (lastError) {
+            msgSpan.innerHTML = Sanitizer.escapeHTML`<span style="color:#dd1111">${lastError.message}</span>`;
+            return;
+          }
+
+          msgSpan.innerHTML = '<span style="color:#1111dd">Configuration has been updated!</span>';
+          setTimeout(function() {
+            msgSpan.innerHTML = '';
+          }, 2500);
+        });
+    } catch (e) {
+      msgSpan.innerHTML = '<span style="color:#dd1111">Failed to save because of invalid format!</span>';
+    }
+  }
+
+  const booleanSettings = ['hidesHistory', 'hidesAccountId', 'showOnlyMatchingRoles', 'autoAssumeLastRole'];
+  for (let key of booleanSettings) {
+    elById(`${key}CheckBox`).onchange = function() {
+      chrome.storage.sync.set({ [key]: this.checked });
+    }
+  }
+
+  elById('configSenderIdText').onchange = function() {
+    chrome.storage.sync.set({ configSenderId: this.value });
+  }
+
+  chrome.storage.sync.get(['lztext', 'configSenderId'].concat(booleanSettings), function(data) {
+    let rawData = localStorage['rawdata'];
+    if (data.lztext) {
+      try {
+        rawData = LZString.decompressFromUTF16(data.lztext);
+      } catch(err) {
+        rawdata = ';; !!!WARNING!!!\n;; Latest setting is broken.\n;; !!!WARNING!!!\n';
+      }
+    }
+    textArea.value = rawData || '';
+    elById('configSenderIdText').value = data.configSenderId || '';
+    for (let key of booleanSettings) {
+      elById(`${key}CheckBox`).checked = data[key] || false;
+    }
+  });
+}
